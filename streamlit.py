4b6534d76301306101e1f9c23f58001ca232b9cd
Gabe <gmfennema@gmail.com>
2024-12-20 10:56:00 -0700

    adjusted height

--- streamlit.py
+++ streamlit.py
@@ -109,7 +109,7 @@ def plot_activity_heatmap(df, year):
         ]
     ).properties(
         width=600,
-        height=200,
+        height=400,
         title=alt.TitleParams(
             text='Daily Activity Heatmap',
             anchor='middle',  # Center the title
